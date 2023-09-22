09f46a41069cf5507f4e102d8157b30c2c34dd81
Soumalya Pakrashi <31507860+soumalyapakrashi@users.noreply.github.com>
2023-09-22 21:00:38 +0530

    Add multi-select to enable users to pick fruits.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Pull data from AWS S3 bucket and read that using CSV
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 # Display the dataframe
 streamlit.dataframe(my_fruit_list)
