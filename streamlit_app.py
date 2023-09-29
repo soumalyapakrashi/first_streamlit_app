438addcbe75a7c9cb502a60935136da58ded2829
Soumalya Pakrashi <31507860+soumalyapakrashi@users.noreply.github.com>
2023-09-29 11:40:25 +0530

    Take fruit input from user and display it's info

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,11 @@ streamlit.dataframe(fruits_to_show)
 # New section to display FruityVice API response
 streamlit.header("Fruityvice Fruit Advice!")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Take the JSON version of the output and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
